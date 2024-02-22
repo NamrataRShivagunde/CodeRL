9f7599337860c7df5215fa682546bdae38e1dd75
Nammu <namratashivagunde@gmail.com>
2024-02-22 13:43:18 -0500

    fix

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,7 +4,7 @@
 ## SPDX-License-Identifier: BSD-3-Clause
 ## For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 ##
-model_path=Salesforce/codet5-large-ntp-py
+model_path=Salesforce/codet5p-770m
 tokenizer_path=Salesforce/codet5p-770m
 test_path=data/APPS/test/ 
 
