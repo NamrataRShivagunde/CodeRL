af34f1a507707d7d9c72dee7b8af6fecb3889bc4
Nammu <namratashivagunde@gmail.com>
2024-02-22 13:03:55 -0500

    generating APPS samples

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,8 +4,8 @@
 ## SPDX-License-Identifier: BSD-3-Clause
 ## For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 ##
-model_path=models/codet5_finetuned_codeRL
-tokenizer_path=models/codet5_tokenizer/
+model_path=Salesforce/codet5-large-ntp-py
+tokenizer_path=Salesforce/codet5-large-ntp-py
 test_path=data/APPS/test/ 
 
 start=0
